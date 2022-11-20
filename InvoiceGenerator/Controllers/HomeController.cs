9d5e33bb62779a5a4bb1cd7c013498fb87a1cb33
Sebastian <sebastian.siejak@gmail.com>
2022-11-20 13:11:15 +0100

    Change generate PDF method

--- InvoiceGenerator/Controllers/HomeController.cs
+++ InvoiceGenerator/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using InvoiceGenerator.Models;
 using Microsoft.AspNetCore.Mvc;
-using Wkhtmltopdf.NetCore;
+using Razor.Templating.Core;
+using WkHtmlToPdfDotNet;
 
 namespace InvoiceGenerator.Controllers
 {
@@ -8,16 +9,32 @@ namespace InvoiceGenerator.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
-        readonly IGeneratePdf _generatePdf;
-        public HomeController(IGeneratePdf generatePdf)
-        {
-            _generatePdf = generatePdf;
-        }
-
         [HttpPost]
         public async Task<IActionResult> CreatePdf([FromBody] InvoiceData invoiceData)
         {
-            return await _generatePdf.GetPdf("Templates/InvoiceTemplate/InvoiceTemplate.cshtml", invoiceData);
+            var html = await RazorTemplateEngine.RenderAsync("~/Templates/InvoiceTemplate/InvoiceTemplate.cshtml", invoiceData);
+
+            var converter = new BasicConverter(new PdfTools());
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Landscape,
+                    PaperSize = PaperKind.A4Rotated,
+    },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = html,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                    }
+                }
+            };
+
+            byte[] pdf = converter.Convert(doc);
+
+            return File(pdf, "application/pdf", "test.pdf");
         }
     }
 }
---
+++ InvoiceGenerator/Controllers/InvoiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+
+        //[HttpPost]
+        //public ActionResult ExportToPDF()
+        //{
+
+        //}
+    }
+}
